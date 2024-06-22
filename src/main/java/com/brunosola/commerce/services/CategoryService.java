80cbfd047c778cfda4910282bc19dde50f001ab3
BrunoSola <brunosola3@gmail.com>
2024-06-22 17:21:37 -0300

    :sparkles: feat: Criado Endpoint GET categories

---
+++ src/main/java/com/brunosola/commerce/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.brunosola.commerce.services;
+
+import com.brunosola.commerce.dto.CategoryDTO;
+import com.brunosola.commerce.entities.Category;
+import com.brunosola.commerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll(){
+        List<Category> categoryDTOList = categoryRepository.findAll();
+        return categoryDTOList.stream().map(CategoryDTO::new).toList();
+    }
+}
